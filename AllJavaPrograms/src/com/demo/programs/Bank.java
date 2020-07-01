5bd2ec18dc9a020e3405366b42d848dd1a366508
mahadevi <kolkarmahadevi@gmail.com>
2020-07-01 11:02:27 +0530

    Bank Project added

---
+++ AllJavaPrograms/src/com/demo/programs/Bank.java
@@ -0,0 +1,21 @@
+package com.demo.programs;
+
+public class Bank {
+
+	public static void main(String[] args) {
+
+		String AccHolderName="Vidya";
+		long AccNo=10025469123L;
+		String BankName="Syndicate Bank";
+		short IFSC_code=11835;
+		float AccBal=5450.55f;
+
+		System.out.println("\nAccount holder name:"+AccHolderName);
+		System.out.println("\nAccount number:"+AccNo);
+		System.out.println("\nBank name:"+BankName);
+		System.out.println("\nIFSC:"+IFSC_code);
+		System.out.println("\nAccount balance:"+AccBal);
+
+	}
+
+}
