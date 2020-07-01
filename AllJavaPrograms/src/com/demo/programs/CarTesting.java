df5c6351f4b8f93b1acaa7a4a5f93139c9e757e5
mahadevi <kolkarmahadevi@gmail.com>
2020-07-01 10:47:18 +0530

    I had done some chanes

---
+++ AllJavaPrograms/src/com/demo/programs/CarTesting.java
@@ -0,0 +1,19 @@
+package com.demo.programs;
+
+public class CarTesting {
+
+	public static void main(String[] args) {
+		String Car_Name="Toyota";
+		double Car_Price=600000.00;
+		byte No_Of_Seats=4;
+		String Fuel_Type="Petrol";
+	
+		System.out.println("Car Name:"+Car_Name);
+		System.out.println("Car Price:"+Car_Price);
+		System.out.println("Number Of Seats:"+No_Of_Seats);
+		System.out.println("Fuel Type:"+Fuel_Type);
+		
+
+	}
+
+}
